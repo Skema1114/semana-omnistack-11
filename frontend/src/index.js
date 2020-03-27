497b41354889d5c7113e6c09cad08a7a8514b212
Rafael Aranda Martins <skema1114@hotmail.com>
2020-03-27 01:14:23 -0300

    Finalizando a terceira aula

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,3 +4,9 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+//instalar pacotes dos icones para o react
+//npm install react-icons
+
+//instalar o pacote de rotas
+//npm install react-router-dom
+//npm install axios
